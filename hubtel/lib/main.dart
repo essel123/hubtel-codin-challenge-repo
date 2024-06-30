bb68b30015982f46211b9e1160e5165a21943b7a
essel123 <abrahamessel156@gmail.com>
2024-06-30 07:17:56 +0000

    recent

--- hubtel/lib/main.dart
+++ hubtel/lib/main.dart
@@ -11,15 +11,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
-     
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: ListView(
-          children: [
-            
-          ],
-        ),
-      ),
+          body: ListView(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 50, horizontal: 50),
+                child: Container(
+                  height: 40,
+                  decoration: const BoxDecoration(color: Colors.grey),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        height: 25,
+                        width: 100,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                        ),
+                        child: const Center(child: Text("History")),
+                      ),
+                      const Text("Transaction Summary")
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+              selectedItemColor: Colors.black,
+              showSelectedLabels: true,
+              showUnselectedLabels: true,
+              unselectedItemColor: Colors.grey,
+              items: const [
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.home,
+                    ),
+                    label: "Home"),
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.send_to_mobile_rounded,
+                    ),
+                    label: "Send"),
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.horizontal_split_rounded,
+                    ),
+                    label: "History"),
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.calendar_today,
+                    ),
+                    label: "Schedule"),
+              ])),
     );
   }
 }
